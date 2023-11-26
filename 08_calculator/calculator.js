315988be7dfebd3c8e302c33725b44d6d0d73f7d
natureslayr <34704015+natureslayr@users.noreply.github.com>
2023-11-25 22:24:06 -0500

    Upload completed Javascript Exervices

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(x,y) {
+	let answer = x + y;
+  return answer;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	let answer = x - y;
+  return answer
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let answer = 0;
+  if (x.length == 0) {
+    answer = 0;
+  }
+	for (let i = 0; i < x.length; i++) {
+    answer += x[i];
+  }
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  let answer = 1;
+  if (x.length == 0) {
+    answer = 0;
+  }
+  for (let i = 0; i < x.length; i++) {
+    answer = answer * x[i]
+  }
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	let answer = x ** y;
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let answer = 1;
+	for (let i = 1; i <= x; i++) {
+    answer = answer * i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
